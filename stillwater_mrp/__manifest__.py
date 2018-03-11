eae79822cf11c284b5ec6d547b64a642503b994d
Kayur Gajjar <kga@odoo.com>
2018-03-10 16:30:04 -0800

    [ADD] Adjust 'to consume' in MO

---
+++ stillwater_mrp/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Stillwater: Mrp Customizations",
+    'summary': "Web",
+    'description': """
+Stillwater: Mrp Customizations
+==============================
+- Ability to update the 'To Consume' in Consumed Matirials when MO is in confirmed state.
+""",
+    "author": "Odoo Inc",
+    'website': "https://www.odoo.com",
+    'category': 'Custom Development',
+    'version': '0.1',
+    'depends': ['mrp'],
+    'data': [
+        'views/mrp_views.xml'
+    ],
+    'license': 'OEEL-1',
+}
