19600d5758b93ac5ec814a5c24ba8a940cbc9ea9
sna-odoo <sna@odoo.com>
2021-01-19 11:03:36 -0800

    [ADD] Update tax based on product total

--- stillwater_sale_total/models/sale.py
+++ stillwater_sale_total/models/sale.py
@@ -18,4 +18,16 @@ class SaleOrder(models.Model):
                 'amount_tax': amount_tax,
                 'amount_total': amount_untaxed + amount_tax,
             })
-  
+    
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id', 'order_line.x_studio_product_total')
+    def _compute_amount(self):
+        for line in self:
+            price = line.x_studio_product_total * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
+            if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
+                line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
